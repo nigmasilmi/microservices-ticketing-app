d1b222b9ad213383f443f75da0af67129b7cdf8f
nigmasilmi <sn.silmi@gmail.com>
2021-12-04 15:05:25 -0300

    adjustments to ingress-srv.yaml

---
+++ auth/src/app.ts
@@ -0,0 +1,39 @@
+import express from 'express';
+import 'express-async-errors';
+import cookieSession from 'cookie-session';
+import { currentUserRouter } from './routes/current-user';
+import { signinRouter } from './routes/singin';
+import { signoutRouter } from './routes/signout';
+import { signupRouter } from './routes/signup';
+import { errorHandler } from './middlewares/error-handler';
+import { NotFoundError } from './errors/not-found-error';
+
+const app = express();
+// due to ingress, trust the proxy
+app.set('trust proxy', true);
+app.use(express.json());
+app.use(
+  cookieSession({
+    // don't use encryption on the cookie
+    signed: false,
+    // allow connections on https
+    secure: true,
+  })
+);
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
+
+app.all('*', async (req, res) => {
+  throw new NotFoundError();
+});
+app.use(errorHandler);
+
+export { app };
--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,41 +1,5 @@
-import express from 'express';
-import 'express-async-errors';
 import mongoose from 'mongoose';
-import cookieSession from 'cookie-session';
-import { currentUserRouter } from './routes/current-user';
-import { signinRouter } from './routes/singin';
-import { signoutRouter } from './routes/signout';
-import { signupRouter } from './routes/signup';
-import { errorHandler } from './middlewares/error-handler';
-import { NotFoundError } from './errors/not-found-error';
-
-const app = express();
-// due to ingress, trust the proxy
-app.set('trust proxy', true);
-app.use(express.json());
-app.use(
-  cookieSession({
-    // don't use encryption on the cookie
-    signed: false,
-    // allow connections on https
-    secure: true,
-  })
-);
-app.use(
-  express.urlencoded({
-    extended: true,
-  })
-);
-
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signoutRouter);
-app.use(signupRouter);
-
-app.use(errorHandler);
-app.get('*', async (req, res) => {
-  throw new NotFoundError();
-});
+import { app } from './app';
 
 const start = async () => {
   if (!process.env.JWT_KEY) {
