088aa099a01ba54df7d4d10d493516e036b75dff
nigmasilmi <sn.silmi@gmail.com>
2021-12-27 17:01:07 -0300

    Adding base to solve concurrency errors in nats (stan options)

--- nats-test-prj/src/listener.ts
+++ nats-test-prj/src/listener.ts
@@ -13,10 +13,14 @@ stan.on('connect', () => {
     console.log('nats connection closed');
     process.exit();
   });
-  const options = stan.subscriptionOptions().setManualAckMode(true);
+  const options = stan
+    .subscriptionOptions()
+    .setManualAckMode(true)
+    .setDeliverAllAvailable()
+    .setDurableName('orders-service');
   const subscription = stan.subscribe(
     'ticket:created',
-    'orders-service-queue-group',
+    'queue-group-name',
     options
   );
   subscription.on('message', (msg: Message) => {
