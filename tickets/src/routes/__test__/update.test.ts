ff3adf729dd90d67f6201b9f2017499b9348e899
nigmasilmi <sn.silmi@gmail.com>
2022-01-21 19:20:34 -0300

    Tests for publishing events, NATS envs configured

--- tickets/src/routes/__test__/update.test.ts
+++ tickets/src/routes/__test__/update.test.ts
@@ -1,6 +1,7 @@
 import request from 'supertest';
 import { app } from '../../app';
 import mongoose from 'mongoose';
+import { natsWrapper } from '../../nats-wrapper';
 // const id = new mongoose.Types.ObjectId().toHexString();
 
 it('returs a 404 if the provided id does not exist', async () => {
@@ -72,4 +73,19 @@ it('updates a ticket provided valid inputs', async () => {
 
   expect(ticketResponse.body.title).toEqual('A new title');
   expect(ticketResponse.body.price).toEqual(10);
+});
+
+it('publishes an event', async () => {
+  const cookie = global.signin();
+  const response = await request(app)
+    .post(`/api/tickets`)
+    .set('Cookie', cookie)
+    .send({ title: 'Hello world ticket', price: 10 });
+
+  await request(app)
+    .put(`/api/tickets/${response.body.id}`)
+    .set('Cookie', cookie)
+    .send({ title: 'A new title', price: 10 })
+    .expect(200);
+  expect(natsWrapper.client.publish).toHaveBeenCalled();
 });
