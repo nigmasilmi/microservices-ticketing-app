de521158dbd0e07ac59098cb95cac7a4a4a56d57
nigmasilmi <sn.silmi@gmail.com>
2022-01-21 10:05:17 -0300

    Test-Suite Wide Mock for nats-wrapper

--- tickets/src/__mocks__/nats-wrapper.ts
+++ tickets/src/__mocks__/nats-wrapper.ts
@@ -2,8 +2,12 @@ export const natsWrapper = {
   // define the only property that the publisher cares about
   // look for how the real object is used and mock only the necessary
   client: {
-    publish(subject: string, data: string, callback: () => void) {
-      callback();
-    },
+    publish: jest
+      .fn()
+      .mockImplementation(
+        (subject: string, data: string, callback: () => void) => {
+          callback();
+        }
+      ),
   },
 };
