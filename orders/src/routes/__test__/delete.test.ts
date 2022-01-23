1933759f9d07164a3fb1bc2972cbf98fe6b6b877
nigmasilmi <sn.silmi@gmail.com>
2022-01-23 11:41:36 -0300

    Cancelling orders (delete route) and its test suite implemented

--- orders/src/routes/__test__/delete.test.ts
+++ orders/src/routes/__test__/delete.test.ts
@@ -1,1 +1,52 @@
-it.todo('testing delete orders suite');
+import request from 'supertest';
+import { app } from '../../app';
+import { Order, OrderStatus } from '../../model/order';
+import { Ticket } from '../../model/ticket';
+
+// the user is authenticated
+// the order parameter is valid
+// the order exists
+// the user is owner of the order
+// the order can be created and its status updated
+
+it('can only be accesed if the user is signed in', async () => {
+  //
+  await request(app).delete('/api/orders/123').send({}).expect(401);
+});
+
+it('The orderId must be a valid mongoDB id string', async () => {
+  await request(app)
+    .delete('/api/orders/123')
+    .set('Cookie', global.signin())
+    .send()
+    .expect(400);
+});
+
+it('Marks an order as cancelled', async () => {
+  // create a ticket with the Ticket model
+  const ticket = Ticket.build({
+    title: 'zoo',
+    price: 10,
+  });
+
+  await ticket.save();
+
+  const user = global.signin();
+
+  // make a request to create an order
+  const { body: order } = await request(app)
+    .post('/api/orders')
+    .set('Cookie', user)
+    .send({ ticketId: ticket.id })
+    .expect(201);
+  // make a request to cancel the order
+  await request(app)
+    .delete(`/api/orders/${order.id}`)
+    .set('Cookie', user)
+    .send()
+    .expect(204);
+
+  // expectation to make sure the order is cancelled
+  const updatedOrder = await Order.findById(order.id);
+  expect(updatedOrder!.status).toEqual(OrderStatus.Cancelled);
+});
--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -1,9 +1,38 @@
 import express, { Request, Response } from 'express';
+import mongoose from 'mongoose';
+import {
+  NotAuthorizedError,
+  NotFoundError,
+  requireAuth,
+  validateRequest,
+} from '@ns_micros/tickets-common';
+import { param } from 'express-validator';
+import { Order, OrderStatus } from '../model/order';
 
 const router = express.Router();
 
-router.delete('/api/orders/:orderId', async (req: Request, res: Response) => {
-  res.send({});
-});
+router.delete(
+  '/api/orders/:orderId',
+  requireAuth,
+  [
+    param('orderId')
+      .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
+      .withMessage('A valid order id must be provided'),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const { orderId } = req.params;
+    const order = await Order.findById(orderId);
+    if (!order) {
+      throw new NotFoundError();
+    }
+    if (order.userId !== req.currentUser!.id) {
+      throw new NotAuthorizedError();
+    }
+    order.status = OrderStatus.Cancelled;
+    await order.save();
+    res.status(204).send(order);
+  }
+);
 
 export { router as deleteOrderRouter };
