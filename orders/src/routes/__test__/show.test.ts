67dc4522a69664e7eadc1ad765aeab09423143ad
nigmasilmi <sn.silmi@gmail.com>
2022-01-23 11:06:41 -0300

    Controller to show specific order for a specific user and test suite implemented

--- orders/src/routes/__test__/show.test.ts
+++ orders/src/routes/__test__/show.test.ts
@@ -1,1 +1,49 @@
-it.todo('testing show order suite');
+import request from 'supertest';
+import { app } from '../../app';
+import { Ticket } from '../../model/ticket';
+
+it('fetches an order', async () => {
+  //Create a ticket
+  const ticket = Ticket.build({ title: 'zoo', price: 10 });
+  await ticket.save();
+
+  const user = global.signin();
+  // Create an order with the ticket in it
+  const { body: order } = await request(app)
+    .post('/api/orders')
+    .set('Cookie', user)
+    .send({
+      ticketId: ticket.id,
+    })
+    .expect(201);
+  // Request the order
+  const { body: fetchedOrder } = await request(app)
+    .get(`/api/orders/${order.id}`)
+    .set('Cookie', user)
+    .send()
+    .expect(200);
+  // Expect to have an order in the response
+  expect(fetchedOrder.id).toEqual(order.id);
+});
+
+it('returns an error if one user tries to fetch another users order', async () => {
+  //Create a ticket
+  const ticket = Ticket.build({ title: 'zoo', price: 10 });
+  await ticket.save();
+
+  const user = global.signin();
+  // Create an order with the ticket in it
+  const { body: order } = await request(app)
+    .post('/api/orders')
+    .set('Cookie', user)
+    .send({
+      ticketId: ticket.id,
+    })
+    .expect(201);
+  // Request the order with a diferent currentUser
+  await request(app)
+    .get(`/api/orders/${order.id}`)
+    .set('Cookie', global.signin())
+    .send()
+    .expect(401);
+});
--- orders/src/routes/show.ts
+++ orders/src/routes/show.ts
@@ -1,9 +1,34 @@
 import express, { Request, Response } from 'express';
-
+import mongoose from 'mongoose';
+import {
+  NotAuthorizedError,
+  NotFoundError,
+  requireAuth,
+  validateRequest,
+} from '@ns_micros/tickets-common';
+import { Order } from '../model/order';
+import { param } from 'express-validator';
 const router = express.Router();
 
-router.get('/api/orders/:orderId', async (req: Request, res: Response) => {
-  res.send({});
-});
+router.get(
+  '/api/orders/:orderId',
+  requireAuth,
+  [
+    param('orderId')
+      .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
+      .withMessage('A valid order id must be provided'),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const order = await Order.findById(req.params.orderId).populate('ticket');
+    if (!order) {
+      throw new NotFoundError();
+    }
+    if (order.userId !== req.currentUser!.id) {
+      throw new NotAuthorizedError();
+    }
+    res.send(order);
+  }
+);
 
 export { router as showOrderRouter };
