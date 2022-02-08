30f5d20e100c1f021fc0af2f6fa99935307f5241
nigmasilmi <sn.silmi@gmail.com>
2022-02-08 08:55:47 -0300

    Added logic to createChargeRouter

--- payments/src/routes/new.ts
+++ payments/src/routes/new.ts
@@ -5,6 +5,8 @@ import {
   validateRequest,
   BadRequestError,
   NotFoundError,
+  NotAuthorizedError,
+  OrderStatus,
 } from '@ns_micros/tickets-common';
 import { Order } from '../models/order';
 
@@ -16,6 +18,20 @@ router.post(
   [body('token').not().isEmpty(), body('orderId').not().isEmpty()],
   validateRequest,
   async (req: Request, res: Response) => {
+    const { orderId, token } = req.body;
+    //find the order
+    const order = await Order.findById(orderId);
+    if (!order) {
+      throw new NotFoundError();
+    }
+    // verify the order belongs to the user
+    if (order.userId !== req.currentUser!.id) {
+      throw new NotAuthorizedError();
+    }
+    // verify the order has not been cancelled
+    if (order.status === OrderStatus.Cancelled) {
+      throw new BadRequestError('Can not pay for a cancelled order');
+    }
     res.send({ success: true });
   }
 );
